a3be3e1477d738d5d9936894fc6e63176bf68cbf
Ultcrt <zhang_zhe_yuan@outlook.com>
2023-01-29 18:51:48 +0800

    Add recursive and precision options

--- main.py
+++ main.py
@@ -5,23 +5,44 @@ import exifread
 import os
 
 if __name__ == '__main__':
-    photo_directory = input("Please input image directory: ")
-    filenames = os.listdir(photo_directory)
-    print(filenames, "\nDirectory contains files above, are you sure to process? (Y/N)")
-    answer = input("")
-    while answer != "Y" and answer != "N":
+    print("Do you want to classify files recursively? (Y/N)")
+    recursive_flag = input("")
+    while recursive_flag != "Y" and recursive_flag != "N":
         print("Invalid input, Y for yes and N for no: ")
-        answer = input("")
+        recursive_flag = input("")
+
+    print("Do you want to classify files by day, month or year? (Y/M/D)")
+    classify_precision = input("")
+    while classify_precision != "Y" and classify_precision != "M" and classify_precision != "D":
+        print("Invalid input, Y for year, M for month and D for day: ")
+        classify_precision = input("")
+
+    input_directory = input("Please input directory: ")
+    if recursive_flag == "Y":
+        filepaths = []
+        for path, _, filenames in os.walk(input_directory):
+            for filename in filenames:
+                filepaths.append(os.path.join(path, filename))
+    else:
+        filepaths = os.listdir(input_directory)
+
+    print(filepaths, "\nDirectory contains %s files above, are you sure to process? (Y/N)" % len(filepaths))
+    double_check = input("")
+    while double_check != "Y" and double_check != "N":
+        print("Invalid input, Y for yes and N for no: ")
+        double_check = input("")
+
+    output_directory = "./output/"
+
     count = 0
-    if answer == "Y":
+    if double_check == "Y":
         print("Processing...")
-        for filename in filenames:
-            file_path = os.path.join(photo_directory, filename)
+        for filepath in filepaths:
             # Ignore directories
-            if not os.path.isdir(file_path):
+            if not os.path.isdir(filepath):
                 # Ignore .ini files
-                if not filename.endswith(".ini"):
-                    with open(file_path, "rb") as f:
+                if not filepath.endswith(".ini"):
+                    with open(filepath, "rb") as f:
                         exif_info = exifread.process_file(f, stop_tag="EXIF DateTimeOriginal")
                         # Has exif
                         if exif_info.get("EXIF DateTimeOriginal", None) is not None:
@@ -29,16 +50,33 @@ if __name__ == '__main__':
                             taken_time_struct = time.strptime(taken_time_string, "%Y:%m:%d %H:%M:%S")
                         # Doesn't have exif
                         else:
-                            create_timestamp = os.path.getctime(file_path)
-                            modified_timestamp = os.path.getmtime(file_path)
-                            taken_timestamp = modified_timestamp if create_timestamp > modified_timestamp else create_timestamp
+                            create_timestamp = os.path.getctime(filepath)
+                            modified_timestamp = os.path.getmtime(filepath)
+                            taken_timestamp = min(modified_timestamp, create_timestamp)
                             taken_time_struct = time.localtime(taken_timestamp)
-                        year_path = os.path.join(photo_directory, str(taken_time_struct.tm_year))
-                        mon_path = os.path.join(year_path, str(taken_time_struct.tm_mon).zfill(2))
-                        if not os.path.exists(year_path):
-                            os.mkdir(year_path)
-                        if not os.path.exists(mon_path):
-                            os.mkdir(mon_path)
-                        shutil.copy(file_path, mon_path)
+
+                        if classify_precision == "Y":
+                            dest_path = os.path.join(
+                                output_directory,
+                                str(taken_time_struct.tm_year)
+                            )
+                        elif classify_precision == "M":
+                            dest_path = os.path.join(
+                                output_directory,
+                                str(taken_time_struct.tm_year),
+                                str(taken_time_struct.tm_mon).zfill(2)
+                            )
+                        elif classify_precision == "D":
+                            dest_path = os.path.join(
+                                output_directory,
+                                str(taken_time_struct.tm_year),
+                                str(taken_time_struct.tm_mon).zfill(2),
+                                str(taken_time_struct.tm_mday).zfill(2)
+                            )
+
+                        if not os.path.exists(dest_path):
+                            os.makedirs(dest_path, exist_ok=True)
+
+                        shutil.copy2(filepath, dest_path)
                         count += 1
-    print("Finished (%d in total)." % count)
+    print("Finished (moved %d files in total)." % count)
